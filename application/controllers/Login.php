033076810c1a0f674c755b7668aeccb9de9eeee8
jakartait <achmad.maulana.and@gmail.com>
2016-10-30 16:17:19 +0700

    add model

---
+++ application/controllers/Login.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+
+ */
+
+/**
+ * Description of Login
+ *
+ * @author Achmad Maulana
+ */
+class Login extends CI_Controller {
+
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        
+    }
+
+    function signin() {
+        
+    }
+
+    function signout() {
+        
+    }
+
+}
