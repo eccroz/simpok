c548b4d1fb989ae2800b442ef262f30ca9f08800
jakartait <achmad.maulana.and@gmail.com>
2016-10-30 16:17:12 +0700

    add model

---
+++ application/models/Kartu_model.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+ */
+
+/**
+ * Description of Kartu_model
+ *
+ * @author Achmad Maulana
+ */
+class Kartu_model extends CI_Model{
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+}
---
+++ application/models/Login_model.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+ */
+
+/**
+ * Description of Login_model
+ *
+ * @author Achmad Maulana
+ */
+class Login_model extends CI_Model {
+
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
