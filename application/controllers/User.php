4b0f32bc97a7896d2a2277e7d4fe6fceeb2a70d4
jakartait <achmad.maulana.and@gmail.com>
2016-10-30 09:51:06 +0700

    create user controller

---
+++ application/controllers/User.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+    -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE -=  
+-----------------------------------------------------------------------
+ Developed by    : Achmad Maulana
+ License         : Commercial
+ Company         : Jakarta IT
+ Email           : achmad.maulana.and@gmail.com
+ Copyright       : SLC (c) 2015. All Rights Reserved. 
+ Designed and built with all the love and loyalty.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
