737ba83734be4c08760fe958f0bfb02c5c391e58
jakartait <achmad.maulana.and@gmail.com>
2016-10-30 09:54:42 +0700

    create user controller

---
+++ application/models/Dokter_model.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ *     -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE -=  
+ * -----------------------------------------------------------------------
+ *  Developed by    : Achmad Maulana
+ *  License         : Commercial
+ *  Company         : Jakarta IT
+ *  Email           : achmad.maulana.and@gmail.com
+ *  Copyright       : SLC (c) 2015. All Rights Reserved. 
+ *  Designed and built with all the love and loyalty.
+ */
+
+/**
+ * Description of Dokter_model
+ *
+ * @author Achmad Maulana
+ */
+class Dokter_model extends CI_Model{
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+}
