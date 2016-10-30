3beecb24d9abb09dbd31884f438e461af13d8a72
jakartait <achmad.maulana.and@gmail.com>
2016-10-30 09:55:00 +0700

    add model

---
+++ application/models/Menu_model.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ *     -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE -=  
+ * -----------------------------------------------------------------------
+ *  Developed by    : Achmad Maulana
+ *  License         : Commercial
+ *  Company         : Jakarta IT
+ *  Email           : achmad.maulana.and@gmail.com
+ *  Copyright       : SLC (c) 2015. All Rights Reserved. 
+ *  Designed and built with all the love and loyalty.
+ */
+
+/**
+ * Description of Menu_model
+ *
+ * @author Achmad Maulana
+ */
+class Menu_model extends CI_Model{
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+    
+}
